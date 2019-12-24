15fe0d7bc14b84f714e817b6f938fd3a0c99cf95
angian <andrea.giancola@gmail.com>
2019-12-24 23:01:55 +0100

    fixed, single-room dungeon generator

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,9 +3,12 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
-## [Unreleased]
+
+
+## [v0.1] 2019-12-24
 
 ### Added
 - README with installation instructions and list of pertinent online resources
 - Pipenv dependency file
+- single-room, fixed dungeon generator
 
--- giangen.py
+++ giangen.py
@@ -1,27 +1,49 @@
 #!/usr/bin/env python3
 
 from PIL import Image
+from PIL import ImageDraw
 
-
+###### params ######
 img_path = "./output/maze_00.png"
-img_w = 800
-img_h = 600
+world_w = 20
+world_h = 15
+
+room_size = 5
+tile_size = 40
+
+bkg_color = "black"
+room_color = "white"
+
+####################
+
 
 
 def main():
-	print(" ------------ giangen ------------")
-	print(" ---- a Python maze generator ----")
+	print()
+	print(" -- giangen --")
+	print(" a Python maze generator")
 	print()
 
-	save_image(img_path)
+	img = init_image()
+	draw_rooms(img)
+	save_image(img, img_path)
 	print("saved image as {}".format(img_path))
 
 	print()
 
 
+def init_image():
+	return Image.new('RGB', (world_w*tile_size, world_h*tile_size), color = bkg_color)
 
-def save_image(img_path):
-	img = Image.new('RGB', (img_w, img_h), color = 'red')
+def draw_rooms(img):
+	draw = ImageDraw.Draw(img)
+	x_start = 3
+	y_start = 5
+	draw.rectangle(((x_start*tile_size, y_start*tile_size), 
+		((x_start+room_size)*tile_size, (y_start+room_size)*tile_size)), 
+		fill=room_color)
+
+def save_image(img, img_path):
 	img.save(img_path)
 
 
